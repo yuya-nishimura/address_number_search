c8a1f6424a29024e77ffbe3724a5f8383e71b6cb
Yuya Nishimura <y-nishimura@r-learning.co.jp>
2019-12-21 21:54:52 +0900

    feature: search.rb実装

--- lib/search.rb
+++ lib/search.rb
@@ -4,8 +4,51 @@ def search(input)
   queries = input.to_bigram
   queries.uniq!
 
-  # インデックスファイルを開きクエリで検索し、マッチしたものを表示していく
-  CSV.foreach(INDEX_PATH, encoding: "UTF-8") do |row|
-    puts row.join(",") if row[1..3].address_matched?(queries)
+  # インデックスをパース
+  index_hash = parse_index(INDEX_PATH)
+
+  # 必要な行数を取得、見つからなければ終了
+  row_numbers = fetch_row_numbers(index_hash, queries)
+  return "該当するレコードは存在しません" if row_numbers.empty?
+
+  # データソースを展開
+  puts "CSVを開いています..."
+  records = CSV.readlines(DATASRC_PATH, encoding: "SJIS:UTF-8")
+
+  # 結果を表示する
+  puts search_results(records, row_numbers)
+end
+
+# インデックスのJSONをパースするメソッド
+def parse_index(index_path)
+  puts "インデックスを開いています..."
+  index_hash = {}
+  File.open(index_path) do |f|
+    index_hash = JSON.load(f)
   end
+  index_hash
+end
+
+# クエリ配列からインデックスを検索し取り出すべき行数をまとめるメソッド
+def fetch_row_numbers(index_hash, queries)
+  puts "インデックスを検索しています..."
+  # インデックスから、キーがクエリにマッチする値を取り出す(行数の配列)
+  row_numbers = []
+  queries.each do |query|
+    row_numbers << index_hash[query]
+  end
+  # 該当する行数を一つの配列にまとめる
+  row_numbers.flatten.compact.uniq
+end
+
+# 結果表示用の配列を返すメソッド
+def search_results(records, row_numbers)
+  puts "結果を表示します..."
+  results = []
+  # 該当する行から郵便番号、都道府県名、市町村区名、町域を出力する
+  row_numbers.each do |row_number|
+    record = records[row_number]
+    results << record[2] + "," + record[6..8].join(",")
+  end
+  results
 end
--- test/search_test.rb
+++ test/search_test.rb
@@ -2,17 +2,40 @@ require "minitest/autorun"
 require_relative "../lib/search.rb"
 
 require "json"
+require "csv"
 
 # searchメソッドのテスト
 class SearchTest < Minitest::Test
   INDEX_PATH = File.expand_path("../index.json", __dir__)
+  DATASRC_PATH = File.expand_path("../src/KEN_ALL.CSV", __dir__)
+  SAMPLE_INDEX_HASH = { "東京" => [0, 1], "京都" => [0, 1],
+                        "杉並" => [0], "並区" => [0],
+                        "本天" => [0], "天沼" => [0],
+                        "中野" => [1], "野区" => [1] }
 
-  # インデックスのパーステスト
-  def test_search
-    index_hash = {}
-    File.open(INDEX_PATH) do |f|
-      index_hash = JSON.load(f)
-    end
+  # parse_indexメソッドのテスト
+  def test_parse_index
+    index_hash = parse_index(INDEX_PATH)
+    # File.open(INDEX_PATH) do |f|
+    #   index_hash = JSON.load(f)
+    # end
     assert_equal [124_351, 124_352], index_hash["那国"] # インデックスが巨大なのでここだけテスト
+  end
+
+  # fetch_row_numbersメソッドのテスト
+  def test_fetch_row_numbers
+    assert_equal [0, 1], fetch_row_numbers(SAMPLE_INDEX_HASH, %w[東京 京都])
+    assert_equal [0], fetch_row_numbers(SAMPLE_INDEX_HASH, %w[本天 天沼])
+    assert_equal [1], fetch_row_numbers(SAMPLE_INDEX_HASH, %w[中野 野区])
+    assert_equal [], fetch_row_numbers(SAMPLE_INDEX_HASH, %w[ほげ ほげ])
+  end
+
+  # search_resultsメソッドのテスト
+  def test_search_results
+    records = CSV.readlines(DATASRC_PATH, encoding: "SJIS:UTF-8")
+    expected_str1 = "0982202,北海道,中川郡美深町,玉川"
+    expected_str2 = "0368375,青森県,弘前市,細越"
+    expected_results = [expected_str1, expected_str2]
+    assert_equal expected_results, search_results(records, [5435, 8636])
   end
 end
